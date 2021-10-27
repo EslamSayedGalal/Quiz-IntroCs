178b2c0ebfa572866c18f737fca651ff250c6db4
Islam Sayed Galal Mohamed <76924322+EslamSayedGalal@users.noreply.github.com>
2021-10-26 18:26:29 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,222 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "What is the brain of the computer?",
+        answer: "CPU",
+        options: [
+            "Motherboard",
+            "Memory",
+            "NIC",
+            "CPU"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Everything physical in a computer is attached to the... ?",
+        answer: "Motherboard",
+        options: [
+            "Hard drive",
+            "CPU",
+            "Motherboard",
+            "Memory"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Application software...?",
+        answer: "Let's the user perform a task.",
+        options: [
+            "Tells the computer components what to do",
+            "Let's the computer interact with the user",
+            "Let's the user perform a task.",
+            "Is encoded on a piece of hardware."
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does GUI stand for....",
+        answer: "Graphical User Interface",
+        options: [
+            "Graphical User Interim",
+            "Graphical User Interface",
+            "Geographical User Interruption",
+            "Gain Upper Intensity"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What allows multiple programs to run on a computer?",
+        answer: "OS",
+        options: [
+            "OS",
+            "NIC",
+            "API",
+            "GUI"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What type of software is free to try but then you have to buy?",
+        answer: "Shareware",
+        options: [
+            "Commercial",
+            "Opensource",
+            "Shareware",
+            "Microsoft Word"
+        ]
+    },
+    {
+        numb: 7,
+        question: "How do programs make request to the Operating System?",
+        answer: "API",
+        options: [
+            "GUI",
+            "TCP",
+            "SMB",
+            "API"
+        ]
+    },
+    {
+        numb: 8,
+        question: "The Miss Porters Network is a ....?",
+        answer: "LAN",
+        options: [
+            "MAN",
+            "SAN",
+            "LAN",
+            "WAN"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Every Network Interface Card has a _______ address, which is a unique identifier?",
+        answer: "MAC",
+        options: [
+            "GUI",
+            "MAC",
+            "NIC",
+            "CPU"
+        ]
+    },
+    {
+        numb: 10,
+        question: "When was the world wide we invented?",
+        answer: "1991",
+        options: [
+            "1974",
+            "1982",
+            "1991",
+            "1999"
+        ]
+    },
+    {
+        numb: 11,
+        question: "Information that is broken down into small chunks before it is sent to another computer are called...",
+        answer: "Packets",
+        options: [
+            "Packets",
+            "Packages",
+            "Messages",
+            "Letters"
+        ]
+    },
+    {
+        numb: 12,
+        question: "Which is considered to be the computers short-term memory?",
+        answer: "RAM",
+        options: [
+            "REM",
+            "ROM",
+            "RIM",
+            "RAM"
+        ]
+    },
+    {
+        numb: 13,
+        question: "What are the four major components of a computer?",
+        answer: "Input Devices, Processing Unit, Storage, Output Devices",
+        options: [
+            "Input Devices, CPU, Storage, Output Devices",
+            "Input Devices, Processing Unit, Storage, Output Devices",
+            "Motherboard, Processing Unit, Storage, Output Devices",
+            "Input Devices, Processing Unit, Memory, Output Devices"
+        ]
+    },
+    {
+        numb: 14,
+        question: "Which one has two examples of output devices?",
+        answer: "Speaker and printer",
+        options: [
+            "Monitor and scanner",
+            "Speaker and keyboard",
+            "Printer and scanner",
+            "Speaker and printer"
+        ]
+    },
+    {
+        numb: 15,
+        question: "A digital circuit capable of holding a single digit?",
+        answer: "Flip-flop",
+        options: [
+            "Pixel",
+            "Bit",
+            "Key field",
+            "Flip-flop"
+        ]
+    },
+    {
+        numb: 16,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 17,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 18,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 19,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 20,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+];
